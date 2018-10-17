beec18b020712ae48b7ce186fab488c6272e7199
Chris Addy <chris.william.addy@gmail.com>
2018-10-16 22:17:41 -0400

    add data

--- philly.py
+++ philly.py
@@ -1,29 +1,26 @@
 from scraper import phetch_philly
-import pandas as pd
 
-### urls are in kind of inconsistent formats
-### easy enough to just list them for now
+# urls are in kind of inconsistent formats
+# easy enough to just list them for now
 
-year_urls = [
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2007%20YTD/Explorer.Transactions.2007ytd.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2008%20YTD/Explorer.Transactions.2008ytd.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2009%20YTD/Explorer.Transactions.2009.YTD.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2010%20YTD/Explorer.Transactions.2010%20.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2011%20YTD/Explorer.Transactions.2011.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2012%20YTD/Explorer.Transactions.2012.YTD.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2013%20YTD/Explorer.Transactions.2013.YTD.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2014%20YTD/Explorer.Transactions.2014.YTD.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2015%20YTD/Explorer.Transactions.2015.YTD.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2016%20YTD/Explorer.Transactions.2016.YTD.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2017%20YTD/Explorer.Transactions.2017.YTD.txt',
-    'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2018%20YTD/Explorer.Transactions.txt'
-    ]
-
-transactions = phetch_philly(year_urls[0])
-
-for url in year_urls[1:]:
-    transactions.append(phetch_philly(url))
-
-print(transactions.info())
-
-### TODO put it in a database
+year_urls = {
+    '2007': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2007%20YTD/Explorer.Transactions.2007ytd.txt',
+    '2008': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2008%20YTD/Explorer.Transactions.2008ytd.txt',
+    '2009': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2009%20YTD/Explorer.Transactions.2009.YTD.txt',
+    # '2010': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2010%20YTD/Explorer.Transactions.2010%20.txt',
+    '2011': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2011%20YTD/Explorer.Transactions.2011.txt',
+    '2012': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2012%20YTD/Explorer.Transactions.2012.YTD.txt',
+    '2013': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2013%20YTD/Explorer.Transactions.2013.YTD.txt',
+    '2014': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2014%20YTD/Explorer.Transactions.2014.YTD.txt',
+    '2015': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2015%20YTD/Explorer.Transactions.2015.YTD.txt',
+    '2016': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2016%20YTD/Explorer.Transactions.2016.YTD.txt',
+    '2017': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2017%20YTD/Explorer.Transactions.2017.YTD.txt',
+    '2018': 'ftp://ftp.phila-records.com/Year-to-Date%20Transaction%20Files/2018%20YTD/Explorer.Transactions.txt'
+    }
+for year, url in year_urls.items():
+    try:
+        print(f'Getting data for {year}')
+        transactions = phetch_philly(url)
+        transactions.to_csv(f'philly_transactions{year}.csv')
+    except:
+        continue
--- scraper.py
+++ scraper.py
@@ -4,5 +4,5 @@ import pandas as pd
 def phetch_philly(url: str) -> pd.DataFrame:
     """Get all year-to-date-transactions for a given year (currently a full url) in data frame form."""
 
-    return pd.read_csv(url, sep = '\t', encoding = 'latin1')
+    return pd.read_csv(url, sep = '\t', encoding = 'latin1', low_memory=False)
 
